181ad4df665b31dfa0f31a65867f3e89c24d44f7
embryzon <95128949+embryzon@users.noreply.github.com>
2024-03-31 11:03:53 +0800

    first commit

---
+++ img_resize.py
@@ -0,0 +1,20 @@
+import os
+import cv2
+from glob import glob
+
+imgs = glob('.\\img\\image_sequence\\*.png')
+output_dir = '.\\img\\360p_bad_apple\\'
+os.makedirs(output_dir, exist_ok=True)
+
+# 1080p: 1440, 1080
+# 720p: 1280, 720
+# 360p: 640, 360 
+
+for img_path in imgs:
+    img = cv2.imread(img_path)
+    img_prime = cv2.resize(img, (640, 360))
+    filename = os.path.split(img_path)[-1]
+    print(filename)
+
+    output_path = os.path.join(output_dir, filename)
+    cv2.imwrite(output_path, img_prime)
---
+++ main.py
@@ -0,0 +1,48 @@
+import cv2
+import matplotlib.pyplot as plt
+from glob import glob
+import os
+
+def get_black_px(bin_img): 
+    black_px = []
+    for x in range(bin_img.shape[0]):
+        for y in range(bin_img.shape[1]):
+            if bin_img[x,y] == 0:
+                black_px.append((x,y))
+    return black_px
+
+imgs = sorted(glob('.\\img\\360p_bad_apple\\*.png'), key= lambda x: int(x.split('\\')[-1].split('_')[-1].split('.')[0]))
+
+fig, ax = plt.subplots()
+
+output_dir = '.\\render\\frames\\'
+os.makedirs(output_dir, exist_ok=True)
+
+img_len = len(imgs)
+for i, img_path in enumerate(imgs):
+    ax.clear()
+
+    img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+    _, bin_img = cv2.threshold(img, 128, 255, cv2.THRESH_BINARY)
+    black_px = get_black_px(bin_img)
+    x = [-x[0] for x in black_px]
+    y = [y[1] for y in black_px]
+
+    # Update the scatter plot data
+    ax.scatter(y, x, s=0.1, color='#000')
+    ax.set_title(f"frame {i:04d}")
+
+    # Save figure
+    filename = f"frame_{i:04d}.png"
+    print(f"{i:04d}/{img_len}")
+
+    output_path = os.path.join(output_dir, filename)
+    plt.savefig(output_path)
+    
+plt.close()
+
+os.system("ffmpeg -framerate 30 -i ./render/frames/frame_%04d.png -i ./img/bad_apple.wav -c:v libx264 -pix_fmt yuv420p ./render/output.mp4")
+
+for i in range(len(img)-1):
+    os.remove(f'./render/frames/frame_{i:04d}.png')
+
